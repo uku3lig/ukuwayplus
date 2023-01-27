91607ff55b183b414096c4498745ed7f66254b55
abby redwood <lostanddead.but.better@gmail.com>
2023-01-27 18:17:22 +0000

    Update NametagMixin.java

--- src/main/java/dev/abodactyl/cce/mixins/NametagMixin.java
+++ src/main/java/dev/abodactyl/cce/mixins/NametagMixin.java
@@ -1,8 +1,25 @@
 package dev.abodactyl.cce.mixins;
 
+import dev.abodactyl.cce.CCEClient;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.w3c.dom.Text;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Text.class) // change but i forgor what this should be
+@Mixin(PlayerListHud.class)
 public class NametagMixin {
+  
+    @Inject(at = @At("RETURN"), method = "getPlayerName", cancellable = true)
+    public void getDisplayName(CallbackInfoReturnable<Text> cir) {
+        Text prev = cir.getReturnValue();
+        if (CCEClient.connected()) {
+            // Ask WebSocket if player is using mod
+            // If so, append rank icon to their username            
+        }
+    }
+  
 }
