abd6bb87658b04620936bf058bfc61077e267bb8
uku <uku3lig@gmail.com>
2023-10-04 19:56:54 +0200

    use drawSlot instead of render in MixinHandledScreen

--- src/main/java/net/uku3lig/ukuway/mixin/MixinHandledScreen.java
+++ src/main/java/net/uku3lig/ukuway/mixin/MixinHandledScreen.java
@@ -1,42 +1,30 @@
 package net.uku3lig.ukuway.mixin;
 
-import net.uku3lig.ukuway.UkuwayPlus;
-import net.uku3lig.ukuway.config.UkuwayConfig;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.TextColor;
-import org.spongepowered.asm.mixin.Final;
+import net.uku3lig.ukuway.UkuwayPlus;
+import net.uku3lig.ukuway.config.UkuwayConfig;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HandledScreen.class)
 public abstract class MixinHandledScreen {
-
-    @Final @Shadow protected ScreenHandler handler;
-    @Shadow protected int x;
-    @Shadow protected int y;
-
-    @Inject(method = "render", at = @At("TAIL"))
-    public void renderSlotRarity(DrawContext drawContext, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+    @Inject(method = "drawSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawItem(Lnet/minecraft/item/ItemStack;III)V"))
+    public void drawSlotRarity(DrawContext context, Slot slot, CallbackInfo ci) {
         if (UkuwayPlus.isConnected() && UkuwayConfig.get().isInventoryRarities()) {
-            for (Slot slot : this.handler.slots) {
-                TextColor itemColor = slot.getStack().getName().getStyle().getColor();
-                if (itemColor != null) {
-                    int colorAlpha = itemColor.getRgb() | (150 << 24);
-                    int leftX = x + slot.x;
-                    int leftY = y + slot.y;
+            TextColor itemColor = slot.getStack().getName().getStyle().getColor();
+            if (itemColor != null) {
+                int argb = itemColor.getRgb() | (150 << 24);
 
-                    drawContext.fill(leftX, leftY + 2, leftX + 1, leftY + 14, colorAlpha);
-                    drawContext.fill(leftX + 1, leftY + 1, leftX + 2, leftY + 15, colorAlpha);
-                    drawContext.fill(leftX + 2, leftY, leftX + 14, leftY + 16, colorAlpha);
-                    drawContext.fill(leftX + 14, leftY + 1, leftX + 15, leftY + 15, colorAlpha);
-                    drawContext.fill(leftX + 15, leftY + 2, leftX + 16, leftY + 14, colorAlpha);
-                }
+                context.fill(slot.x, slot.y + 2, slot.x + 1, slot.y + 14, argb);
+                context.fill(slot.x + 1, slot.y + 1, slot.x + 2, slot.y + 15, argb);
+                context.fill(slot.x + 2, slot.y, slot.x + 14, slot.y + 16, argb);
+                context.fill(slot.x + 14, slot.y + 1, slot.x + 15, slot.y + 15, argb);
+                context.fill(slot.x + 15, slot.y + 2, slot.x + 16, slot.y + 14, argb);
             }
         }
     }
